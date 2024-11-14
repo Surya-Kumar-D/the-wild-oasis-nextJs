31bb8a8c03cf8a6391efd09bafb289d963cd10dd
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-14 21:00:26 +0530

    first commit

--- app/_components/Counter.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useState } from "react";
-
-export default function Counter({ users }) {
-  const [count, setCount] = useState(0);
-  console.log(users);
-  return <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>;
-}
